b656efccce4fae05dfd2baff1188b71e91900fa7
aajhh <a.jameshargreaves@gmail.com>
2025-01-31 18:36:20 +0000

    header

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
              document.getElementById("header-logo").style.visibility = "visible";
              document.getElementById("myHeader").style.borderBottomStyle = "double";
     } else {
