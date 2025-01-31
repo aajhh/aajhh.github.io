c6a035a990d8c99ab1c97b14b28e309e28e85fc4
aajhh <a.jameshargreaves@gmail.com>
2025-01-31 16:28:47 +0000

    header

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -1,9 +1,4 @@
-$(window).on('scroll', function () {
-    if ($(this).scrollTop() > 70) { // Set position from top
-        $('.headernav').addClass("shrink");
-        //$('.navbar-brand  img').attr('src', 'images/logo.png');
-    }else{
-        $('.headernav').removeClass("shrink");
-        //$('.navbar-brand  img').attr('src', 'images/logo.png');
-    }
-    });
+function myFunction() {
+    var element = document.getElementById("header-title-logo");
+    element.classList.add("shrink");
+  } 
