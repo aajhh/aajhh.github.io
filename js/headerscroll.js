0f9055e39cbf405c7e32ca1d11dd474ca69a39ef
aajhh <a.jameshargreaves@gmail.com>
2025-01-31 16:48:20 +0000

    Update headerscroll.js

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
              document.getElementById("header-logo").style.visibility = "visible";
     } else {
          document.getElementById("header-logo").style.visibility = "hidden";
