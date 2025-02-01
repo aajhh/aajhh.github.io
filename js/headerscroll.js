dc4f3baa1a3919f45337adf4b730720b8d7a9d97
aajhh <a.jameshargreaves@gmail.com>
2025-02-01 18:49:50 +0000

    header

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -5,10 +5,12 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
              document.getElementById("header-logo").style.visibility = "visible";
+             document.getElementById("logotext").style.visibility = "hidden";
              document.getElementById("myHeader").style.borderBottomStyle = "solid";
              mybutton.style.display = "block";
     } else {
          document.getElementById("header-logo").style.visibility = "hidden";
+         document.getElementById("logotext").style.visibility = "visible";
          document.getElementById("myHeader").style.borderBottomStyle = "none";
          mybutton.style.display = "none";
     }
