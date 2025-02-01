92435c22f995150f4f341a27583898fc10066f14
aajhh <a.jameshargreaves@gmail.com>
2025-02-01 17:58:29 +0000

    art

--- js/headerscrolllogovis.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-             document.getElementById("myHeader").style.borderBottomStyle = "solid";
-    } else {
-         document.getElementById("myHeader").style.borderBottomStyle = "none";
-    }
-} 
