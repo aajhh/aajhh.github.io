4caf4b1e307197704f9a81e7aabe6541d6eee546
aajhh <a.jameshargreaves@gmail.com>
2025-04-07 14:37:40 +0100

    pdf

--- js/viewer.js
+++ js/viewer.js
@@ -1,111 +1,58 @@
-var _PDF_DOC,
-    _CURRENT_PAGE,
-    _TOTAL_PAGES,
-    _PAGE_RENDERING_IN_PROGRESS = 0,
-    _CANVAS = document.querySelector('#pdf-canvas');
+const PDFStart = nameRoute => {           
+    let loadingTask = pdfjsLib.getDocument(nameRoute),
+        pdfDoc = null,
+        canvas = document.querySelector('#cnv'),
+        ctx = canvas.getContext('2d'),
+        scale = 1.5,
+        numPage = 1;
 
-    // initialize and load the PDF
-async function showPDF(pdf_url) {
-    document.querySelector("#pdf-loader").style.display = 'block';
+        const GeneratePDF = numPage => {
 
-    // get handle of pdf document
-    try {
-        _PDF_DOC = await pdfjsLib.getDocument({ url: pdf_url });
-    }
-    catch(error) {
-        alert(error.message);
-    }
+            pdfDoc.getPage(numPage).then(page => {
 
-    // total pages in pdf
-    _TOTAL_PAGES = _PDF_DOC.numPages;
-    
-    // Hide the pdf loader and show pdf container
-    document.querySelector("#pdf-loader").style.display = 'none';
-    document.querySelector("#pdf-contents").style.display = 'block';
-    document.querySelector("#pdf-total-pages").innerHTML = _TOTAL_PAGES;
+                let viewport = page.getViewport({ scale: scale });
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+                
+                let renderContext = {
+                    canvasContext : ctx,
+                    viewport:  viewport
+                }
 
-    // show the first page
-    showPage(1);
+                page.render(renderContext);
+            })
+            document.querySelector('#npages').innerHTML = numPage;
+
+        }
+
+        const PrevPage = () => {
+            if(numPage === 1){
+                return
+            }
+            numPage--;
+            GeneratePDF(numPage);
+        }
+
+        const NextPage = () => {
+            if(numPage >= pdfDoc.numPages){
+                return
+            }
+            numPage++;
+            GeneratePDF(numPage);
+        }
+
+        document.querySelector('#prev').addEventListener('click', PrevPage)
+        document.querySelector('#next').addEventListener('click', NextPage )
+
+        loadingTask.promise.then(pdfDoc_ => {
+            pdfDoc = pdfDoc_;
+            document.querySelector('#npages').innerHTML = pdfDoc.numPages;
+            GeneratePDF(numPage)
+        });
 }
 
-// load and render specific page of the PDF
-async function showPage(page_no) {
-    _PAGE_RENDERING_IN_PROGRESS = 1;
-    _CURRENT_PAGE = page_no;
-
-    // disable Previous & Next buttons while page is being loaded
-    document.querySelector("#pdf-next").disabled = true;
-    document.querySelector("#pdf-prev").disabled = true;
-
-    // while page is being rendered hide the canvas and show a loading message
-    document.querySelector("#pdf-canvas").style.display = 'none';
-    document.querySelector("#page-loader").style.display = 'block';
-
-    // update current page
-    document.querySelector("#pdf-current-page").innerHTML = page_no;
-    
-    // get handle of page
-    try {
-        var page = await _PDF_DOC.getPage(page_no);
-    }
-    catch(error) {
-        alert(error.message);
-    }
-
-    // original width of the pdf page at scale 1
-    var pdf_original_width = page.getViewport(1).width;
-    
-    // as the canvas is of a fixed width we need to adjust the scale of the viewport where page is rendered
-    var scale_required = _CANVAS.width / pdf_original_width;
-
-    // get viewport to render the page at required scale
-    var viewport = page.getViewport(scale_required);
-
-    // set canvas height same as viewport height
-    _CANVAS.height = viewport.height;
-
-    // setting page loader height for smooth experience
-    document.querySelector("#page-loader").style.height =  _CANVAS.height + 'px';
-    document.querySelector("#page-loader").style.lineHeight = _CANVAS.height + 'px';
-
-    var render_context = {
-        canvasContext: _CANVAS.getContext('2d'),
-        viewport: viewport
-    };
-        
-    // render the page contents in the canvas
-    try {
-        await page.render(render_context);
-    }
-    catch(error) {
-        alert(error.message);
-    }
-
-    _PAGE_RENDERING_IN_PROGRESS = 0;
-
-    // re-enable Previous & Next buttons
-    document.querySelector("#pdf-next").disabled = false;
-    document.querySelector("#pdf-prev").disabled = false;
-
-    // show the canvas and hide the page loader
-    document.querySelector("#pdf-canvas").style.display = 'block';
-    document.querySelector("#page-loader").style.display = 'none';
+const startPdf = () => {
+    PDFStart('../images/zines/house.pdf')
 }
 
-// click on "Show PDF" buuton
-document.querySelector("#show-pdf-button").addEventListener('click', function() {
-    this.style.display = 'none';
-    showPDF('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf');
-});
-
-// click on the "Previous" page button
-document.querySelector("#pdf-prev").addEventListener('click', function() {
-    if(_CURRENT_PAGE != 1)
-        showPage(--_CURRENT_PAGE);
-});
-
-// click on the "Next" page button
-document.querySelector("#pdf-next").addEventListener('click', function() {
-    if(_CURRENT_PAGE != _TOTAL_PAGES)
-        showPage(++_CURRENT_PAGE);
-});
+window.addEventListener('load', startPdf);
